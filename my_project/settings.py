9829539146c5e4be071bd428f30ea3f6fc2bcef3
vitorramosdasilva <rsvitor_81@hotmail.com>
2020-07-22 16:55:32 -0300

    mudanca_template

--- my_project/settings.py
+++ my_project/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'my_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
