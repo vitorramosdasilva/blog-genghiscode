a3e849b921b42333ec1bc1d3efaacd7a43580b13
vitorramosdasilva <rsvitor_81@hotmail.com>
2020-07-23 18:17:45 -0300

    pythonanywhere sqllite

--- my_project/settings.py
+++ my_project/settings.py
@@ -124,23 +124,23 @@ USE_TZ = True
 # django_heroku.settings(locals())
 
 # Python Whenere ..
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# Em Dev ....
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# Em Dev ....
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'blog',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Static files (CSS, JavaScript, Images)
